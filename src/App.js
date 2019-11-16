acc5c69b890ba3833b540809f80fce8b42fd839c
Andrew G <andrewgly95@gmail.com>
2019-11-16 11:58:57 -0500

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+import { StyleSheet, Text, View, FlatList } from "react-native"
+
+class App extends React.Component {
+	state = {
+		StarWarsAPI: [
+			{
+				id: 1,
+				name: "Luke Skywalker"
+			},
+			{
+				id: 2,
+				name: "C-3PO"
+			},
+			{
+				id: 3,
+				name: "R2-D2"
+			}
+		]
+	}
+
+	_keyExtractor = (item, index) => item.id
+
+	_renderItem = ({ item }) => (
+		<View style={styles.listContainer}>
+			<Text style={styles.listItem}>{item.name}</Text>
+		</View>
+	)
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.text}>Hello, world!</Text>
+				<FlatList
+					data={this.state.StarWarsAPI}
+					keyExtractor={this._keyExtractor}
+					renderItem={this._renderItem}
+				/>
+			</View>
+		)
+	}
 }
 
-export default App;
+const styles = StyleSheet.create({
+	container: {
+		padding: 10,
+		flex: 1
+	},
+	text: {
+		fontWeight: "bold",
+		fontSize: 30
+	},
+	listContainer: {
+		paddingTop: 20
+	},
+	listItem: {
+		fontSize: 20
+	}
+})
+
+export default App
